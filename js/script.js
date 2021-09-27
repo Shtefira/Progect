c2c0627b50df18c052fdb23b0baaaf2d9800b8bd
Iren <hm210784sis.cv@gmail.com>
2021-09-27 10:17:12 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+"use strict";
+
+
+//alert ('are you slept well?');
+//console.log (alert);
+
+//const result = confirm ("are you shue?");
+//console.log (result);
+
+//const answer = prompt ("are you married?", "");
+//console.log (typeof(answer));
+//const answer2 = prompt ('Do you know me?','');
+//console.log (typeof(answer2));
+
+// const anwers = [];
+// anwers [0] = prompt (' name?','');
+// anwers [1] = prompt ('surname','');
+// anwers [2] = prompt ('age','');
+// console.log (typeof(anwers));
+
+// const answername = prompt ('What is your nme?');
+
+// const place = prompt ('where are you from?');
+// alert(`Hello, ${answername}, nice to meet you!${place} its a great place!`);
+
+//console.log (2+2*2!=='6');
+
+
+const a = false,
+      c = false,
+      b = false;  
+console.log (a||b||!c);
