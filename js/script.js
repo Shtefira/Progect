689a9dca2d18b079a0bd8a9fb71c0de425faa14b
Iren <hm210784sis.cv@gmail.com>
2021-10-06 11:40:08 +0300

    Labratory work 2 final

--- js/script.js
+++ js/script.js
@@ -2,14 +2,12 @@
 
 let numberOfFilms;
 
-function start (){
+function start(){
     numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели?','');
-
-    while (numberOfFilms == null || numberOfFilms == '' || isNaN(numberOfFilms)){
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
         numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели?','');
     }
 }
-
 start();
 
 const personalMovieDB ={
@@ -19,27 +17,25 @@ const personalMovieDB ={
     genres : [],
     privat : false
 };
- 
-function rememberMyFilms () {
-        for (let i=0; i<2; i++){
-            const answer1 = prompt ('Один из последних просмотренных фильмов?',''),
-                  answer2 = +prompt ('На сколько вы его оцените?', '');
-                   
-                  //Первый способ цикла
-                  if (answer1 != null && answer2 != null && answer1 != '' && answer2 != '' && answer1.length < 50 ) {
-                        personalMovieDB.movies[answer1] = answer2;
-                        console.log ('done');
-                    }   else {
-                        console.log ('error');
-                        i--;
-                   }
+
+function rememberMyFilms (){
+    for (let i=0; i<2; i++){
+        const answer1 = prompt ('Один из последних просмотренных фильмов?',''),
+              answer2 = +prompt ('На сколько вы его оцените?', '');
+               
+              //Первый способ цикла
+              if (answer1 != null && answer2 != null && answer1 != '' && answer2 != '' && answer1.length < 50 ) {
+                    personalMovieDB.movies[answer1] = answer2;
+                    console.log ('done');
+                }   else {
+                    console.log ('error');
+                    i--;
+               }
             }
+}
+rememberMyFilms ();
 
-
- }
- rememberMyFilms();
-
- function detectPersonalLevel(){
+function detectPersonalLevel(){
     if (personalMovieDB.count < 10){
         alert ('Просмотрено довольно мало фильмов!');
     } else if (personalMovieDB.count <= 30){
@@ -49,99 +45,30 @@ function rememberMyFilms () {
        } else {
            alert ('Произошла ошибка!');
        }
-
 }
 detectPersonalLevel();
 
 function showMyDB(){
     if (personalMovieDB.privat == false){
         console.log (personalMovieDB);
+    } else {
+        console.log ('This is privat information');
     }
 }
 showMyDB();
 
-// function writeYourGenres(){
-    
-//     for (let i = 1; i<4; i++){
-//         personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`) ;
-        
-//     }
-// }
-// writeYourGenres ();
-
 function writeYourGenres(){
-    let numGenres = 1;
-    while (numGenres<4){
-        personalMovieDB.genres[ numGenres - 1]= prompt(`Ваш любимый жанр под номером ${numGenres}`) ;
-       numGenres++;
+    for (let i=1; i<=3; i++){
+        personalMovieDB.genres[i - 1] = prompt (`Ваш любимый жанр под номером ${i}`);
+        // const genre = prompt (`Ваш любимый жанр под номером ${i}`);
+        // personalMovieDB.genres[i - 1] = genre;
+
     }
-     
 }
-writeYourGenres ();
-
-// console.log (personalMovieDB); 
-
-
-// Второй вариант
-// function showMyDB(){
-//     (personalMovieDB.privat === false )? console.log (personalMovieDB) : console.log ('error');
-// }
-// showMyDB();
-
-
-        
-          
-  
-   
-
-//второй способ цикла
-    // let i = 0 ;
-    // do { 
-    //     const answer1 = prompt ('Один из последних просмотренных фильмов?',''),
-    //           answer2 = +prompt ('На сколько вы его оцените?', '');
-    //           i++;     
-              
-    //     if (answer1 != null && answer2 != null && answer1 != '' && answer2 != '' && answer1.length < 10 ){
-    //         console.log ('done');
-    //         personalMovieDB.movies[answer1] = answer2;
-           
-    //     } else {
-    //         console.log ('error');
-    //         i--;
-    //     }
+writeYourGenres();
     
 
-    // }
-    // while (i<2);
-
-
-              // Второй способ условий  
-            //   if (answer1 == null){
-            //       console.log ('error');
-            //       i--;
-            //   } else if (answer1 == ''){
-            //     console.log ('error');
-            //     i--;
-            //   } else if  (answer1.length > 10){
-            //     console.log ('error');
-            //     i--;
-            //   } else {
-            //       personalMovieDB.movies[answer1] = answer2;
-            //       console.log ("done");
-                  
-            //   }
-            //   if (answer2 == null) {
-            //       console.log ("error");
-            //       i--;
-            //   }  else if (answer2 == "") {
-            //       console.log ('error');
-            //       i--;
-            //   } else {
-            //       personalMovieDB.movies[answer1] = answer2;
-            //       console.log ("done");
-                  
-                  
        
 
-    
+
 
