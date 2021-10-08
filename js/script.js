6a298309b049942415dbfc5a90296dfc2600a978
Shtefira <hm210784sis.cv@gmail.com>
2021-10-08 15:37:00 +0300

    Lesson 22

--- js/script.js
+++ js/script.js
@@ -1,28 +1,78 @@
 "use strict";
 
-const arr = [101,12,203,16,10];
-arr.sort (comparNum);
-console.log (arr);
-function comparNum (a,b){
-    return a-b;
-}
+// циклы для создания поверхностных копий
 
-// arr.pop ();
-// arr.push (11);
-
-// for (let i=0; i<arr.length; i++){
-//     console.log (arr [i]);
+// function Copy (mainObj){
+//     let objCopy = {};
+//     let key;
+//     for (key in mainObj){
+//         objCopy [key] = mainObj [key];
+//     }
+//     return objCopy;
 // }
+// const numbers = {
+//     a:2,
+//     b:5,
+//     c: {
+//         x:7,
+//         y:4
+//     }
+// };
+// // const newNumbers = Copy (numbers);
 
-// for (let namber of arr){
-//     console.log (namber);
+// // newNumbers.b = 10;
+// // console.log (newNumbers);
+// // console.log (numbers);
+
+
+// //Object.assign
+const add = {
+    d:17,
+    e:20
+};
+const numbers = {
+    a:2,
+    b:5,
+    c: {
+        x:7,
+        y:4
+    }
+};
+// console.log (Object.assign(numbers,add));
+
+const clon = Object.assign ({},add);
+clon.d = 20;
+// console.log (add);
+// console.log (clon);
+
+// const oldArray = ['a','b','c'];
+// const newAarray = oldArray.slice ();
+// newAarray [1] = 'mama';
+// console.log (oldArray);
+// console.log (newAarray);
+
+// const video = ['a','b','c'],
+//       blog = ['d', 'e', 'f'],
+//       internet = [...video,...blog,'q', 'j'];
+//  console.log (internet);
+
+// function logic (a,b,c){
+//     console.log (a);
+//     console.log (b);
+//     console.log (c);
 // }
+// const num = [2, 255, 7];
+// logic (...num);
 
-// arr.forEach (function (item, i, arr){
-//     console.log (`${i} : ${item} incide of array ${arr}`);
-// });
+// const oldArray = ['a','b','c'];
+// const newAarray = [...oldArray];
+// newAarray [0] = 'mama';
+// console.log (newAarray);
 
-// const str = prompt ("Какой товар вас интересует?","");
-// const products = str.split (', ');
-// console.log (products);
-// console.log (products.join (';'));
+const add2 = {
+    d:17,
+    e:20
+};
+const newadd2 = {...add2};
+newadd2.d = 3555;
+console.log (newadd2);
