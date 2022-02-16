5eb4bb1e5beb3535bbeb3fed0c0000cd61cbe96b
Iren Shtefanesa <hm210784sis.cv@gmail.com>
2022-02-16 14:44:28 +0200

    Lesson 60

--- food_OS/js/script.js
+++ food_OS/js/script.js
@@ -41,7 +41,7 @@ window.addEventListener('DOMContentLoaded', function() {
     
     // Timer
 
-    const deadline = '2020-05-11';
+    const deadline = '2023-05-11';
 
     function getTimeRemaining(endtime) {
         const t = Date.parse(endtime) - Date.parse(new Date()),
@@ -181,36 +181,44 @@ window.addEventListener('DOMContentLoaded', function() {
             this.parent.append(element);
         }
     }
-
+//       Первый способ
     // getResource('http://localhost:3000/menu')
     //     .then(data => {
     //         data.forEach(({img, altimg, title, descr, price}) => {
     //             new MenuCard(img, altimg, title, descr, price, ".menu .container").render();
     //         });
     //     });
+//         Второй способ
+    // getResource('http://localhost:3000/menu')
+    //     .then(data => createCard(data));
 
-    getResource('http://localhost:3000/menu')
-        .then(data => createCard(data));
+    // function createCard(data) {
+    //     data.forEach(({img, altimg, title, descr, price}) => {
+    //         const element = document.createElement('div');
 
-    function createCard(data) {
-        data.forEach(({img, altimg, title, descr, price}) => {
-            const element = document.createElement('div');
+    //         element.classList.add("menu__item");
 
-            element.classList.add("menu__item");
+    //         element.innerHTML = `
+    //             <img src=${img} alt=${altimg}>
+    //             <h3 class="menu__item-subtitle">${title}</h3>
+    //             <div class="menu__item-descr">${descr}</div>
+    //             <div class="menu__item-divider"></div>
+    //             <div class="menu__item-price">
+    //                 <div class="menu__item-cost">Цена:</div>
+    //                 <div class="menu__item-total"><span>${price}</span> грн/день</div>
+    //             </div>
+    //         `;
+    //         document.querySelector(".menu .container").append(element);
+    //     });
+    // }
 
-            element.innerHTML = `
-                <img src=${img} alt=${altimg}>
-                <h3 class="menu__item-subtitle">${title}</h3>
-                <div class="menu__item-descr">${descr}</div>
-                <div class="menu__item-divider"></div>
-                <div class="menu__item-price">
-                    <div class="menu__item-cost">Цена:</div>
-                    <div class="menu__item-total"><span>${price}</span> грн/день</div>
-                </div>
-            `;
-            document.querySelector(".menu .container").append(element);
+    axios.get('http://localhost:3000/menu')
+        .then(data=> {
+            data.data.forEach(({img, altimg, title, descr, price}) => {
+            new MenuCard(img, altimg, title, descr, price, ".menu .container").render();
+            });
         });
-    }
+
 
     // Forms
 
