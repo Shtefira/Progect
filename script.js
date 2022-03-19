d331377c3199c411771608152bdb25252c0864a0
Iren Shtefanesa <hm210784sis.cv@gmail.com>
2022-03-19 19:03:34 +0200

    Lesson 69

--- script.js
+++ script.js
@@ -1,57 +1,47 @@
 'use strict';
 
-//filter - для фильтрации данных из одного массива в другой
+class User {
+    constructor(name, age) {
+        this.name = name;
+        this._age = age;
 
-// const names = ['Ivan', 'Iren', 'Ekaterina', 'Evgenia'];
+    }
 
-// const shortNames = names.filter(function(name) {
-//     return name.length < 5;
-//  });
-// console.log (shortNames);
+    #surname = 'Shtefanesa';
 
+    get surname () {
+        return this.#surname;
+    }
 
-//map - для трансформации данных из массива в др. массив
+    set surname (surname) {
+        if (typeof surname === 'string' && surname.length > 10) {
+            this.#surname = surname;
+        } else {
+            alert = 'Недопустимое значение!';
+        }
+    }
+    
+    say = () => {
+        console.log(`Имя пользователя: ${this.name} ${this.#surname}, возраст пользователя :${this._age}`);
 
-// const answers = ['IvAn', 'IreN', 'EkatErina'];
+    }
 
-// const res = answers.map( item => item.toLowerCase());
+    get age () {
+        return this._age;
+    }
 
-// console.log(res);
+    set age (age) {
+        if (typeof age === 'number' && age > 0 && age < 110){
+            this._age = age;
+        } else {
+            console.log('Недопустимое значение!');
+        }
+    }
+}
 
+const ivan = new User ('Ivan', 27);
+console.log(ivan.surname);
+console.log(ivan.surname = 'Pivovar');
+console.log(ivan.surname);
 
-// every -перебирает массив и возвращает нам правда/ложь если ВСЕ из эл. подошли под критерий перебора
-// some - перебирает массив и возвращает нам правда/ложь если хоть ОДИН из эл. подошел под критерий перебора
-
-// const arr = [4, 'Ivan', 'ndfvsf'];
-
-// console.log(arr.some(item => typeof(item) === 'number'));
-
-// console.log(arr.every(item => typeof(item) === 'string'));
-
-
-//reduce - собирает массив в одно единое целое, как бы склеевает и помещает в единое значение
-
-// const arr = [4, 5, 1, 2, 3, 6];
-
-// const res = arr.reduce((sum, number) => sum  + number);
-// console.log(res);
-// console.log(typeof(res));
-
-// const arr = ['Ivan', 'Iren', 'Ekaterina'];
-
-// const res = arr.reduce((sum, number) => `${sum}, ${number}`, 'Shtefanesa');
-// console.log(res);
-
-
-// const obj = {
-//     ivan: 'person',
-//     anna: 'person',
-//     dog: 'animal',
-//     cat: 'animal'
-// };
-
-// const newArr = Object.entries(obj)
-// .filter(item => item[1] === 'person')
-// .map(item => item[0]);
-
-// console.log(newArr);
+ivan.say();
